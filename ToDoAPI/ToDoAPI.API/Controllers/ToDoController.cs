029e3f733470e34e8581e4b308385474c7f481e6
Michael Redifer <mikeredifer@yahoo.com>
2022-03-29 13:14:46 -0500

    Initial commit. API setup and data layer conneciton

---
+++ ToDoAPI/ToDoAPI.API/Controllers/ToDoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToDoAPI.API.Models;//For access to the vm's (Data Transfer Objects)
+using ToDoAPI.DATA.EF;
+using System.Web.Http.Cors;//This allows us to modify CORS permissions to what we need in this app
+
+namespace ToDoAPI.API.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ToDoController : ApiController
+    {
+        ToDoEntities db = new ToDoEntities();
+
+    }
+}
